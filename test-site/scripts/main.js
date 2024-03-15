6e4a62b2f23bd52d5768de0b38347fbce9768737
xapa87 <78208599+xapa87@users.noreply.github.com>
2024-03-15 17:28:51 +0900

    Add files via upload

---
+++ test-site/scripts/main.js
@@ -0,0 +1,34 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/test.png");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("あなたの名前を入力してください。");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = `${myName} さん、Mozilla はかっこいいよ。`;
+    }
+  }
+  
+  if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Mozilla はかっこいいよ、${storedName}`;
+  }
+  myButton.onclick = () => {
+    setUserName();
+  };
+  
